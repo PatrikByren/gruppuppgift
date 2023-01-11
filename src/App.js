9b02bf1d8f3791870e045387734f424fd90cfa41
Patrik Byren <patrik.byren@hotmail.com>
2023-01-11 11:47:07 +0100

    fix misstake

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import Addpoktoplayer from './componets/Addpoktoplayer';
 import './App.css';
 
 function App() {
---
+++ src/componets/addpoktoplayer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Addpoktoplayer = () => {
+    return (
+        <div>
+            addpoktoplayer
+        </div>
+    );
+}
+
+export default Addpoktoplayer;
